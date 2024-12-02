2ad21b33b7f0ddfa3bb64692e01398532682cea1
Vare <dm.va@hotmail.com>
2024-12-02 19:34:57 -0300

    finished starting point

--- frontend/src/modules/account/templates/account-layout.tsx
+++ frontend/src/modules/account/templates/account-layout.tsx
@@ -22,6 +22,7 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
           <div className="flex-1">{children}</div>
         </div>
         <div className="flex flex-col small:flex-row items-end justify-between small:border-t border-gray-200 py-12 gap-8">
+
           <div>
             <h3 className="text-xl-semi mb-4">Got questions?</h3>
             <span className="txt-medium">
@@ -29,12 +30,14 @@ const AccountLayout: React.FC<AccountLayoutProps> = ({
               customer service page.
             </span>
           </div>
+
           <div>
             <UnderlineLink href="/customer-service">
               Customer Service
             </UnderlineLink>
           </div>
-        </div>
+          
+        </div>  
       </div>
     </div>
   )
